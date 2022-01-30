3f7f0ff8b2f7ea0951754e2d97ee8d1cedd213e3
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2022-01-30 11:52:55 +0000

    Include teleporting in the example

--- src/main/java/example/MovingPlayerExampleState.java
+++ src/main/java/example/MovingPlayerExampleState.java
@@ -11,12 +11,15 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.onemillionworlds.tamarin.compatibility.ActionBasedOpenVrState;
 import com.onemillionworlds.tamarin.compatibility.DigitalActionState;
+import com.onemillionworlds.tamarin.vrhands.BoundHand;
+import com.onemillionworlds.tamarin.vrhands.VRHandsAppState;
 import com.onemillionworlds.tamarin.vrhands.grabbing.AutoMovingGrabControl;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.Label;
@@ -31,6 +34,7 @@ public class MovingPlayerExampleState extends BaseAppState{
     Node rootNodeDelegate = new Node("BlockMovingExampleState");
 
     ActionBasedOpenVrState openVr;
+    VRHandsAppState vrHands;
 
     public MovingPlayerExampleState(){
     }
@@ -39,12 +43,14 @@ public class MovingPlayerExampleState extends BaseAppState{
     protected void initialize(Application app){
         ((SimpleApplication)app).getRootNode().attachChild(rootNodeDelegate);
         openVr = getState(ActionBasedOpenVrState.class);
+        vrHands = getState(VRHandsAppState.class);
         initialiseScene();
     }
 
     @Override
     protected void cleanup(Application app){
         rootNodeDelegate.removeFromParent();
+        getApplication().getCamera().setLocation(new Vector3f(0,0,10));
     }
 
     @Override
@@ -60,16 +66,40 @@ public class MovingPlayerExampleState extends BaseAppState{
     @Override
     public void update(float tpf){
         super.update(tpf);
+        //the "camera" is not the real camera, it is acting as the origin on the VR space (that the player then walks about in)
+        Camera observer = getApplication().getCamera();
 
         DigitalActionState leftAction = openVr.getDigitalActionState("/actions/main/in/turnLeft", null);
         if (leftAction.changed && leftAction.state){
-            //the "camera" is not the real camera, it is acting as the origin on the VR space (that the player then walks about in)
             Quaternion currentRotation = getApplication().getCamera().getRotation();
             Quaternion leftTurn = new Quaternion();
             leftTurn.fromAngleAxis(0.2f*FastMath.PI, Vector3f.UNIT_Y);
 
-            getApplication().getCamera().setRotation(leftTurn.mult(currentRotation));
+            observer.setRotation(leftTurn.mult(currentRotation));
         }
+
+        DigitalActionState rightAction = openVr.getDigitalActionState("/actions/main/in/turnRight", null);
+        if (rightAction.changed && rightAction.state){
+
+            Quaternion currentRotation = getApplication().getCamera().getRotation();
+            Quaternion leftTurn = new Quaternion();
+            leftTurn.fromAngleAxis(-0.2f*FastMath.PI, Vector3f.UNIT_Y);
+
+            observer.setRotation(leftTurn.mult(currentRotation));
+        }
+
+        //although we have by default bound teleport to the left hand the player may have redefined it, so check both
+        for(BoundHand boundHand : vrHands.getHandControls()){
+            DigitalActionState teleportAction = openVr.getDigitalActionState("/actions/main/in/teleport", boundHand.getHandSide().restrictToInputString);
+            if (teleportAction.changed && teleportAction.state){
+                //teleport in the direction the hand that requested it is pointing
+                Vector3f pointingDirection = boundHand.getBulkPointingDirection();
+                pointingDirection.y=0;
+                observer.setLocation(observer.getLocation().add(pointingDirection.mult(2)));
+            }
+        }
+
+
 
     }
 
@@ -84,10 +114,10 @@ public class MovingPlayerExampleState extends BaseAppState{
 
         //a lemur UI with text explaining what to do
         Container lemurWindow = new Container();
-        lemurWindow.setLocalScale(0.01f); //lemur defaults to 1 meter == 1 pixel (because that make sense for 2D, scale it down, so it's not huge in 3d)
-        Label label = new Label("Use the left hat forward to teleport forwards, left and right to sharply turn\n\nUse the right hat forward to walk (very nausea inducing)\n\nIn a real game you'd want some indicator to show where you were teleporting to. \n\nThe turn left and right is to help with seated experiences where physically turning 360 may be annoying or impossible");
+        lemurWindow.setLocalScale(0.02f); //lemur defaults to 1 meter == 1 pixel (because that make sense for 2D, scale it down, so it's not huge in 3d)
+        Label label = new Label("Use the left hat forward to teleport forwards, left and right to sharply turn\n\nUse the right hat forward to walk (very nausea inducing)\n\nIn a real game you'd want some indicator to show where you were teleporting to. \n\nThe turn left and right is to help with seated experiences where physically turning 360 may be annoying or impossible. \n\n Teleport off the checkerboard to exit");
         lemurWindow.addChild(label);
-        lemurWindow.setLocalTranslation(0,5,0);
+        lemurWindow.setLocalTranslation(-5,2,0);
         rootNodeDelegate.attachChild(lemurWindow);
     }
 
