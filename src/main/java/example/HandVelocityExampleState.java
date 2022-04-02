5c8ae51ceb4b69bfebbb223130b6de3e6e8742ac
Richard Tingle <98174673+oneMillionWorlds@users.noreply.github.com>
2022-04-02 15:27:22 +0100

    Add some gravity affected stones for throwing, just for fun

--- src/main/java/example/HandVelocityExampleState.java
+++ src/main/java/example/HandVelocityExampleState.java
@@ -39,9 +39,9 @@ public class HandVelocityExampleState extends BaseAppState{
     VRHandsAppState vrHands;
 
     Vector3f stoneCentre = new Vector3f(0,0.75f, 9.4f);
-    Vector3f stoneTarget = new Vector3f(5,1.5f, 5);
+    Vector3f stoneTarget = new Vector3f(4,1.5f, 4);
 
-    float targetHalfSize = 0.4f;
+    float targetHalfSize = 0.5f;
 
     List<Geometry> stones = new ArrayList<>();
 
@@ -140,7 +140,7 @@ public class HandVelocityExampleState extends BaseAppState{
         if (needNewStones){
             for(int i = -2; i <= 2; i++){
                 for(int j = 0; j < 2; j++){
-                    Geometry stone = createStoneToPickUp(stoneCentre.add(i * 0.2f, j*0.2f, 0));
+                    Geometry stone = createStoneToPickUp(stoneCentre.add(i * 0.2f, j*0.2f, 0), j == 0);
                     rootNodeDelegate.attachChild(stone);
                     stones.add(stone);
                 }
@@ -171,7 +171,7 @@ public class HandVelocityExampleState extends BaseAppState{
         //a lemur UI with text explaining what to do
         Container lemurWindow = new Container();
         lemurWindow.setLocalScale(0.02f); //lemur defaults to 1 meter == 1 pixel (because that make sense for 2D, scale it down, so it's not huge in 3d)
-        Label label = new Label("Try throwing 'stones', you can also apply spin to them. Try moving (joysticks) while holding a stone \n\nBe careful not to throw the controller though ;)\n\nTo exit throw a stone at the red box");
+        Label label = new Label("Try throwing 'stones', you can also apply spin to them. Try moving (joysticks) while holding a stone \n\nBe careful not to throw the controller though ;)\n\nRed stones are affected by gravity, grey ones aren't\n\nTo exit throw a stone at the red box");
         lemurWindow.addChild(label);
         lemurWindow.setLocalTranslation(-5,4,0);
         rootNodeDelegate.attachChild(lemurWindow);
@@ -183,25 +183,40 @@ public class HandVelocityExampleState extends BaseAppState{
      * from then on
      * @return a Geometry that is a stone
      */
-    private Geometry createStoneToPickUp(Vector3f location){
+    private Geometry createStoneToPickUp(Vector3f location, boolean gravityAffected){
         Box box = new Box(0.05f, 0.05f, 0.05f);
         Geometry boxGeometry = new Geometry("box", box);
         Material boxMat = new Material(getApplication().getAssetManager(),"Common/MatDefs/Misc/Unshaded.j3md");
         boxMat.setTexture("ColorMap", getApplication().getAssetManager().loadTexture("Textures/stone.png"));
+
+        if (gravityAffected){
+            boxMat.setColor("Color", ColorRGBA.Red);
+        }
         boxGeometry.setMaterial(boxMat);
 
         boxGeometry.setLocalTranslation(location);
         AutoMovingGrabControl grabAndThrowControl = new AutoMovingGrabControl(new Vector3f(0.025f,0,0), 0.05f){
+            boolean physicsActivated = false;
+
             Vector3f velocity = new Vector3f();
             RotationalVelocity angularVelocity = new RotationalVelocity(new Vector3f());
 
             @Override
             protected void controlUpdate(float tpf){
                 super.controlUpdate(tpf);
+
+                if (this.getGrabbingHand().isPresent() || !physicsActivated){
+                    return; //don't apply "physics" to objects being held, the hand will take care of it
+                }
+
                 Spatial spatial = getSpatial();
                 if (spatial.getWorldTranslation().y<0){
                     spatial.removeFromParent();
                     stones.remove(spatial);
+                }
+
+                if (gravityAffected){
+                    velocity.y-=9.8*tpf;
                 }
 
                 spatial.setLocalTranslation(spatial.getLocalTranslation().add(velocity.mult(tpf)));
@@ -216,6 +231,7 @@ public class HandVelocityExampleState extends BaseAppState{
                 super.onRelease(handUnbindingFrom);
                 velocity = handUnbindingFrom.getVelocity_world();
                 angularVelocity = handUnbindingFrom.getRotationalVelocity_world();
+                physicsActivated = true;
             }
         };
         boxGeometry.addControl(grabAndThrowControl);
